af040bbf6703dcf34374ccf181445e4a73d77608
himetani <takafumi_t1224@jcom.home.ne.jp>
2018-03-19 15:11:45 +0900

    Tweak table view

--- table/table.go
+++ table/table.go
@@ -12,13 +12,13 @@ func Render(results []ssh.Result) {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Destination", "Result"})
 	table.SetRowLine(true)
-	table.SetHeaderColor(tablewriter.Colors{tablewriter.FgHiRedColor}, tablewriter.Colors{tablewriter.FgHiRedColor})
+	//table.SetHeaderColor(tablewriter.Colors{tablewriter.FgHiRedColor}, tablewriter.Colors{tablewriter.FgHiRedColor})
 
 	for _, r := range results {
 		if r.Err != nil {
-			table.Append([]string{fmt.Sprintf("%s@%s:%s", r.Dest.User, r.Dest.Host, r.Dest.Port), "🙅♀️Invalid Host"})
+			table.Append([]string{fmt.Sprintf("%s@%s:%s", r.Dest.User, r.Dest.Host, r.Dest.Port), "[X] Not copied"})
 		} else {
-			table.Append([]string{fmt.Sprintf("%s@%s:%s", r.Dest.User, r.Dest.Host, r.Dest.Port), "🙆PubKey is Copied"})
+			table.Append([]string{fmt.Sprintf("%s@%s:%s", r.Dest.User, r.Dest.Host, r.Dest.Port), "[o] Already Copied"})
 		}
 	}
 	table.Render()
