a6f4913f9c02cc6dfe5deb3ea9681d4987bf3f52
himetani <takafumi_t1224@jcom.home.ne.jp>
2018-03-28 08:51:09 +0900

    Add more test

--- ssh/key_test.go
+++ ssh/key_test.go
@@ -2,6 +2,8 @@ package ssh
 
 import (
 	"errors"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -12,14 +14,20 @@ func TestNewPrivateKey(t *testing.T) {
 		Err      error
 	}
 
+	dummyPath := filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "himetani", "ssh-pubkey-copy", "ssh", "testdata", "dummy_id_rsa")
+
 	tests := []data{
-		{"Invalid file", "hoge", errors.New("open hoge: no such file or directory")},
+		{"Invalid filepath", "hoge", errors.New("open hoge: no such file or directory")},
+		{"Invalid file", dummyPath, errors.New("ssh: no key found")},
 	}
 
 	for i, test := range tests {
 		_, err := NewPrivateKey(test.Path)
 		if test.Err == nil {
-			continue
+			if err != nil {
+				t.Errorf("Test #%d %s: Unexpected error happend. err expected nil, but got non-nil. msg=%s.", i, test.TestName, err.Error())
+				continue
+			}
 		}
 
 		if test.Err != nil {
@@ -42,8 +50,11 @@ func TestNewPublicKeyContent(t *testing.T) {
 		Err      error
 	}
 
+	dummyPath := filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "himetani", "ssh-pubkey-copy", "ssh", "testdata", "dummy_id_rsa.pub")
+
 	tests := []data{
-		{"Invalid file", "hoge", errors.New("open hoge: no such file or directory")},
+		{"Invalid filepath", "hoge", errors.New("open hoge: no such file or directory")},
+		{"Valid file", dummyPath, nil},
 	}
 
 	for i, test := range tests {
